a9ef8b36beec22ba8337586cd143c81fd3af03e2
Saumajit Malakar <saumajit15sam@gmail.com>
2025-07-29 12:29:38 +0530

    Add Spring Boot project structure with MySQL setup

---
+++ src/test/java/com/saumajit/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saumajit.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
